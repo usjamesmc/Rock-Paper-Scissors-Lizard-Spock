38c78fd98e41612d513adfd2cc1c6b6e1ce4cae3
michael-nevius <michael.a.nevius@gmail.com>
2022-04-06 12:07:34 -0400

    intial commit try two

---
+++ main.py
@@ -0,0 +1,13 @@
+#RSPLS
+# Display Rules
+# Display a welcome message
+# List of gestures that play can use game?
+# How many players will be playing the game?
+# Ai or human player
+# Battle phase.
+# Best 2 out of 3
+# Who will attack first?
+# Account for bad user input
+# Establish a function to show what class beats what class.
+# Display Winner
+
