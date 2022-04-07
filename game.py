637b2516dfc32acab46fc42d2e4560c90ab5e308
Nicholas Wolf <nicholaswolf@Nicholass-MBP.attlocal.net>
2022-04-07 19:50:39 -0400

    fixed round error and infinite loop

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ class Game:
     def __init__(self):
         self.player_one = Player('Bob')
         self.player_two = Player('Frank')
-
+        self.game_number = 1
 
     def run_game(self):
         self.display_welcome()
@@ -16,8 +16,10 @@ class Game:
         self.display_winner()
         self.end_game()
 
+    def display_welcome(self):
+        self.user_input = input('Welcome to Rock Paper Scissors Lizard Spock, please choose between 1 or 2 players: ')
+
     def game_mode(self):
-        
         while self.user_input != '1' or self.user_input != '2':
             if self.user_input == '1':
                 # print('RPSLS Rules')
@@ -26,7 +28,8 @@ class Game:
                 # print('Scissors beats Paper and Lizard')
                 # print('Lizard beats Paper Spock')
                 # print('Spock beats Rock and Scissors')
-                self.player_vs_computer()
+                self.player_two = Computer('Steve Jobs')
+                self.player_selection()
                 
             elif self.user_input == '2':
                 # print('RPSLS Rules')
@@ -35,27 +38,12 @@ class Game:
                 # print('Scissors beats Paper and Lizard')
                 # print('Lizard beats Paper Spock')
                 # print('Spock beats Rock and Scissors')
-                self.player_vs_player()
+                self.player_two = Player("Frank")
+                self.player_selection()
          
             else:
                 print('Not a valid response')    
                 self.display_welcome()    
-            
-    
-    def display_welcome(self):
-        self.user_input = input('Welcome to Rock Paper Scissors Lizard Spock, please choose between 1 or 2 players: ')
-        
-
-    def player_vs_player(self):
-        self.player_two = Player('Frank')
-        if self.player_one.score < 2 and self.player_two.score < 2:
-            self.display_game_info()
-            self.player_one.choose_gesture()
-            self.player_two.choose_gesture()
-            print(f'{self.player_one.name} chose {self.player_one.chosen_gesture} and {self.player_two.name} chose {self.player_two.chosen_gesture}')
-            self.game_phase(self.player_one.chosen_gesture, self.player_two.chosen_gesture)
-        else:
-            self.display_winner()
           
           
 
@@ -80,26 +68,33 @@ class Game:
         else:
             print(f'{self.player_one.name} wins the round.')
             self.player_one.score += 1
+
     def display_score(self): 
         print(f'{self.player_one.name} has {self.player_one.score} points and {self.player_two.name} has {self.player_two.score} points.')
 
-    def player_vs_computer(self):
-        self.player_two = Computer('Steve Jobs')
-        if  self.player_one.score < 2 and self.player_two.score < 2:
+    def player_selection(self):
+        if self.player_one.score < 2 or self.player_two.score < 2:
+            if self.player_one.score == 2:
+                self.display_winner()
+                self.end_game()
+                
+            elif self.player_two.score == 2:
+                self.display_winner()
+                self.end_game()
+            
             self.display_game_info()
             self.player_one.choose_gesture()
             self.player_two.choose_gesture()
             print(f'{self.player_one.name} chose {self.player_one.chosen_gesture} and {self.player_two.name} chose {self.player_two.chosen_gesture}')
             self.game_phase(self.player_one.chosen_gesture, self.player_two.chosen_gesture)
-        else:
-            self.display_winner()
+            self.player_selection()
+
                      
     
     def display_game_info(self):
-        game_number = 1
-        print(f'Round {game_number}')
+        print(f'Round {self.game_number}')
         print(f'{self.player_one.name}: {self.player_one.score} pts VS {self.player_two.name}: {self.player_two.score} pts.')
-        game_number += 1
+        self.game_number += 1
     
           
         
@@ -112,6 +107,7 @@ class Game:
         
     def end_game(self):
         print('Thanks for playing')
+        exit()
 
         
 
