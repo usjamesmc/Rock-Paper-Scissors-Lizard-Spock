7e2a380eab9a43457ba12442004686fa68414fb9
michael-nevius <michael.a.nevius@gmail.com>
2022-04-07 11:25:15 -0400

    The game is functional however it's an endless loop

--- game.py
+++ game.py
@@ -10,16 +10,17 @@ class Game:
 
     def run_game(self):
         self.display_welcome()
-        self.game_phase()
         self.display_winner()
-        self.display_score()
+        
     
     def display_welcome(self):
         user_input = input('Choose between 1 or 2 players: ')
         print('Welcome to Rock Paper Scissors Lizard Spock') 
         if user_input == '1':
+            self.player_vs_computer()
             print('You will be facing a computer')
         else:
+            self.player_vs_player()
             print('You will be facing a human')
 
     
@@ -28,13 +29,10 @@ class Game:
         while self.player_one.score < 2 and self.player_two.score < 2:
             self.player_one.choose_gesture()
             self.player_two.choose_gesture()
-            self.game_phase(self.player_one.current_gesture, self.player_two.current_gesture)
+            self.game_phase(self.player_one.chosen_gesture, self.player_two.chosen_gesture)
             self.display_score()
        
-    def display_score(self): 
-        print(f'{self.player_one.name} has {self.player_one.score} points and {self.player_two.name} has {self.player_two.score} points.')
-
-
+    
     def game_phase(self, player_one_choice, player_two_choice):
         if player_one_choice == 'Rock' and player_two_choice in ['Paper', 'Spock']:
             print(f'{self.player_two.name} wins the round.')
@@ -54,11 +52,13 @@ class Game:
         elif player_one_choice == player_two_choice:
             print('Both players chose the same gesture its a tie')                                        
         else:
-            self.display_winner
-            
+            print(f'{self.player_one} wins the round.')
+            self.player_one.score += 1
+    def display_score(self): 
+        print(f'{self.player_one.name} has {self.player_one.score} points and {self.player_two.name} has {self.player_two.score} points.')        
         
     def display_winner(self):
-        if self.player_one_score == 2:
+        if self.player_one.score > self.player_two.score:
             print(f'{self.player_one.name}, wins the game')
         else:
             print(f'{self.player_two.name}, wins the game')
--- player.py
+++ player.py
@@ -3,21 +3,21 @@ class Player():
         self.name = name
         self.gesture = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
         self.score = 0
-        self.chosen_gesture = None
+        self.chosen_gesture = []
 
     def choose_gesture(self):
         for gesture in self.gesture:
-            print(f'{self.gestures.index(gesture)}: {gesture}')
+            print(f'{self.gesture.index(gesture)}: {gesture}')
 
         while True:
             try:
-                user_input = int(input('Choose a gesture by number :'))
+                user_input = int(input('Choose a gesture by number: '))
             except ValueError:
                     print("That is not a valid repsonse.")
                     continue
             else:
-                if user_input <= len(self.gestures) - 1:
-                    self.current_gesture = self.gestures[int(user_input)]
+                if user_input <= len(self.gesture) - 1:
+                    self.current_gesture = self.gesture[int(user_input)]
 
                 else:
                     print("That is not a valid repsonse.")
